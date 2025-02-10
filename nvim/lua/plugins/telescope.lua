a3ee59b9e62a5e7c6ddb44b23dae263641fc293e
peter <53406799+SuperNove02@users.noreply.github.com>
2025-02-10 16:02:48 +0100

    changed fing files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
 		},
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		end,
 	},
